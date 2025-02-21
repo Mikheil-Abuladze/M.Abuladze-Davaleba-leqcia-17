66acf48313439cafac8c4008162e488cff116648
Mikheil-Abuladze <abuladzemikheil1995@gmail.com>
2025-02-21 12:36:24 +0400

    finished fifth question

--- indes.js
+++ indes.js
@@ -22,3 +22,27 @@ for (let i = 0; i < numbers.length; i++) {
 }
 console.log(`Even numbers in an array are: ${even}`);
 // ====================
+let day;
+switch (new Date().getDay()) {
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+    day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case 6:
+    day = "Saturday";
+}
+console.log(day);
