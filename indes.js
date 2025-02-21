0974c0540c160adfcec89a36872af6f1e1f12b3e
Mikheil-Abuladze <abuladzemikheil1995@gmail.com>
2025-02-21 11:46:39 +0400

    1,2 questions done

---
+++ indes.js
@@ -0,0 +1,15 @@
+for (i = 1; i <= 14; i++) {
+  console.log(i);
+}
+var input = 1;
+while (input <= 19) {
+  console.log(input);
+  input++;
+}
+let i = 3;
+
+// do...while loop
+do {
+  console.log(i);
+  i--;
+} while (i > 0);
