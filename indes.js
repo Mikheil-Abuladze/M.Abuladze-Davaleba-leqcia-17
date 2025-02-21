9f154ce06ef2971071264e4870ab8b9cdf8f8ccb
Mikheil-Abuladze <abuladzemikheil1995@gmail.com>
2025-02-21 12:04:51 +0400

    finished 4 th question

--- indes.js
+++ indes.js
@@ -2,14 +2,23 @@ for (i = 1; i <= 14; i++) {
   console.log(i);
 }
 var input = 1;
+// /===================/
 while (input <= 19) {
   console.log(input);
   input++;
 }
-let i = 3;
+// =====================
+let b = 24;
 
-// do...while loop
 do {
-  console.log(i);
-  i--;
-} while (i > 0);
+  console.log(b);
+  b--;
+} while (b > 0);
+// ======================
+let numbers = [4, 5, 6, 7, 8, 9, 23, 24, 25, 26, 29];
+let even = [];
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] % 2 == 0) even.push(numbers[i]);
+}
+console.log(`Even numbers in an array are: ${even}`);
+// ====================
